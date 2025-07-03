540fa803b9608f53b8cf266bf25e1215288c1ddf
Renato Andrade <217669951+andrendevelops@users.noreply.github.com>
2025-07-03 08:39:36 -0500

    Edit

--- HWS-Checkpoint-1.playground/Contents.swift
+++ HWS-Checkpoint-1.playground/Contents.swift
@@ -1,3 +1,7 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// Hacking with Swift - Checkpoint 1
+
+let celsius = 38.2
+let fahrenheit = celsius * 9 / 5 + 32
+print("The temperature for today is \(celsius)° celsius or \(fahrenheit)° fahrenheit")

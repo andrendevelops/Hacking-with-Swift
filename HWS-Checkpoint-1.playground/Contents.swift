f35398d7cc85c97ee4d7518aaabc9e80b2ecd032
Renato Andrade <217669951+andrendevelops@users.noreply.github.com>
2025-07-03 16:42:00 -0500

    Edit

--- HWS-Checkpoint-1.playground/Contents.swift
+++ HWS-Checkpoint-1.playground/Contents.swift
@@ -2,6 +2,10 @@ import Cocoa
 
 // Hacking with Swift - Checkpoint 1
 
+// 1. Create a constant holding any temperature in Celsius.
+// 2. Convert it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+// 3. Print the result for the user, showing both the Celsius and Fahrenheit values.
+
 let celsius = 38.2
 let fahrenheit = celsius * 9 / 5 + 32
 print("The temperature for today is \(celsius)° celsius or \(fahrenheit)° fahrenheit")
